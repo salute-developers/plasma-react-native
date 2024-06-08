9719dcff571ae17ee7b513cec0000b8e5b4f0800
Neretin Artem <hawktrike@gmail.com>
2024-06-08 16:54:56 +0500

    feat(core-components): Add core `Checkbox` for user libraries

---
+++ core/core-components/src/components/Checkbox/Checkbox.types.ts
@@ -0,0 +1,118 @@
+import { ViewProps } from 'react-native';
+
+import { Style } from './Checkbox.styles';
+
+interface CustomCheckboxProps extends Omit<ViewProps, 'style'> {
+    /**
+     * Объект для стилизации компонента
+     */
+    style?: Style;
+    /**
+     * Определяет уникальное имя элемента формы
+     */
+    name?: string;
+    /**
+     * Помечен ли такой элемент формы, как флажок или переключатель
+     */
+    checked?: boolean;
+    /**
+     * Значение компонента чекбокс
+     */
+    value?: string;
+    /**
+     * Уникальный идентификатор контрола
+     */
+    id?: string;
+    /**
+     * Метка-подпись к элементу
+     */
+    label?: React.ReactNode;
+    /**
+     * Описание элемента
+     */
+    description?: React.ReactNode;
+    /**
+     * Label и description в одну строку или с переносом строк
+     */
+    singleLine?: boolean;
+    /**
+     * Неопределенное состояние компонента - когда часть потомков не выбрана.
+     */
+    indeterminate?: boolean;
+    /**
+     * Может ли фокусироваться чекбокс
+     */
+    focused?: boolean;
+    /**
+     * Чекбокс неактивен
+     */
+    disabled?: boolean;
+    /**
+     * Вид чекбокса
+     */
+    view?: string;
+    /**
+     * Размер чекбокса
+     */
+    size?: string;
+    /**
+     * Обработчик изменения состояния чекбокса
+     */
+    onValueChange?: (value: boolean) => void;
+}
+
+export interface CheckboxProps extends CustomCheckboxProps {}
+
+export interface CheckboxConfig {
+    defaults: {
+        view: string;
+        size: string;
+        focused: string;
+    };
+    variations: {
+        view: {
+            [x: string]: {
+                fillColor: string;
+                iconColor: string;
+                labelColor: string;
+                descriptionColor: string;
+                triggerBorderColor: string;
+            };
+        };
+        size: {
+            [x: string]: {
+                margin: number;
+                triggerMargin: number;
+                triggerSize: number;
+                triggerBorderRadius: number;
+                triggerBorderWidth: number;
+                triggerEllipseBorderWidth?: number;
+                contentTopOffset: number;
+                contentLeftOffset: number;
+                labelFontFamilyRef: string;
+                labelFontSize: number;
+                labelFontStyle: string;
+                labelFontWeight: string;
+                labelLetterSpacing: number;
+                labelLineHeight: number;
+                descriptionMarginTop: number;
+                descriptionFontFamilyRef: string;
+                descriptionFontSize: number;
+                descriptionFontStyle: string;
+                descriptionFontWeight: string;
+                descriptionLetterSpacing: number;
+                descriptionLineHeight: number;
+            };
+        };
+        disabled: {
+            [x: string]: {
+                disabledOpacity: number;
+            };
+        };
+        focused: {
+            [x: string]: {
+                focusColor: string;
+            };
+        };
+    };
+}
--- core/core-components/src/index.ts
+++ core/core-components/src/index.ts
@@ -1,6 +1,7 @@
 export * from './types';
 export * from './utils';
 export * from './components/Button';
+export * from './components/Checkbox';
 export * from './components/Spinner';
 export * from './components/Progress';
 export * from './components/ThemeProvider';
