8049a29af7a6cdcffcdee0f1f0c264c08bc3edac
Neretin Artem <hawktrike@gmail.com>
2024-06-10 16:43:52 +0500

    feat(plasma-b2c): Add `Radiobox` component

--- libraries/plasma-b2c/src/index.prod.ts
+++ libraries/plasma-b2c/src/index.prod.ts
@@ -2,4 +2,5 @@ export * from './components/Button';
 export * from './components/Checkbox';
 export * from './components/Spinner';
 export * from './components/Progress';
+export * from './components/Radiobox';
 export * from './components/ThemeProvider';
