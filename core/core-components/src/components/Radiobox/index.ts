b61ea5faf09c17f589678d851e312161f591561d
Neretin Artem <hawktrike@gmail.com>
2024-06-10 16:43:16 +0500

    feat(core-components): Add core `Radiobox` for user libraries

---
+++ core/core-components/src/components/Radiobox/index.ts
@@ -0,0 +1,1 @@
+export { radioboxCore, radioboxComponent } from './Radiobox';
--- core/core-components/src/index.ts
+++ core/core-components/src/index.ts
@@ -4,4 +4,5 @@ export * from './components/Button';
 export * from './components/Checkbox';
 export * from './components/Spinner';
 export * from './components/Progress';
+export * from './components/Radiobox';
 export * from './components/ThemeProvider';
