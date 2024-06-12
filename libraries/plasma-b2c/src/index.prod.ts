9bf11f784ab7dacc2a60b5b1bb4c83c540f41c46
Neretin Artem <hawktrike@gmail.com>
2024-06-12 19:37:56 +0500

    feat(plasma-b2c): Add `Switch` component

--- libraries/plasma-b2c/src/index.prod.ts
+++ libraries/plasma-b2c/src/index.prod.ts
@@ -1,6 +1,7 @@
 export * from './components/Button';
 export * from './components/Checkbox';
 export * from './components/Spinner';
+export * from './components/Switch';
 export * from './components/Progress';
 export * from './components/Radiobox';
 export * from './components/ThemeProvider';
