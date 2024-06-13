3594c545cbbc98316a0c3f74ee965ad91587b104
Neretin Artem <hawktrike@gmail.com>
2024-06-13 19:44:25 +0500

    feat(plasma-b2c): Add `Typography` components

--- libraries/plasma-b2c/src/index.prod.ts
+++ libraries/plasma-b2c/src/index.prod.ts
@@ -4,4 +4,5 @@ export * from './components/Spinner';
 export * from './components/Switch';
 export * from './components/Progress';
 export * from './components/Radiobox';
+export * from './components/Typography';
 export * from './components/ThemeProvider';
