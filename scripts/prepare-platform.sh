9d6dad40aa2bdb6f94ebfb8c54d317217da588a4
Neretin Artem <hawktrike@gmail.com>
2024-08-07 13:03:18 +0300

    feat(core-components,plasma-b2c): Add scripts and package.mobile, package.tv for multiplatform build

---
+++ scripts/prepare-platform.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+CORE_COMPONTENS_DIR="core/core-components"
+
+LIBRARIES_DIR="libraries"
+
+prepare_platform() {
+  if [ "$PLATFORM" == 'tv' ] 
+      then
+        cd $1
+
+        if [ -f "package.json" ]; then
+          echo "Package.json in $1 is exist. Starting update"
+          npm run package-update
+        fi
+
+        echo "Create package.json in $1"
+        cp ./platforms/tv/package.json package.json && cp ./platforms/tv/package-lock.json package-lock.json
+
+      else
+        cd $1
+
+        if [ -f "package.json" ]; then
+          echo "Package.json in $1 is exist. Starting update"
+          npm run package-update
+        fi
+
+        echo "Create package.json in $1"
+        cp ./platforms/mobile/package.json package.json && cp ./platforms/mobile/package-lock.json package-lock.json
+  fi
+}
+
+prepare_platform $CORE_COMPONTENS_DIR && cd ../../
+
+for dir in $(find $LIBRARIES_DIR -mindepth 1 -maxdepth 1 -type d | sort -u); do
+  prepare_platform $dir
+done
+
+
