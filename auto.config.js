7a7e617f82114e75923358d3067921842bf90b44
Neretin Artem <hawktrike@gmail.com>
2024-09-10 14:31:48 +0500

    feat: Update packages

---
+++ auto.config.js
@@ -0,0 +1,13 @@
+const npmOptions = {
+    setRcToken: false,
+    commitNextVersion: true,
+};
+
+/** Auto configuration */
+module.exports = function rc() {
+    const plugins = [['npm', npmOptions], 'conventional-commits'];
+
+    return {
+        plugins,
+    };
+};
