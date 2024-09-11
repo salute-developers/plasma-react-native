6de0edab84485cd2643017c8e09785e4be609604
Neretin Artem <hawktrike@gmail.com>
2024-09-11 19:48:41 +0500

    feat: Add scripts for prepare CI/CD processes

---
+++ scripts/set-lerna-packages.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+FILE_PATH="./lerna.json"
+
+ALL_PACKAGES='"packages": ["core/*", "libraries/*", "packages/*", "themes/*"],'
+THEME_PACKAGES='"packages": ["core/core-themes", "themes/*"],'
+COMPONENT_PACKAGES='"packages": ["core/core-components", "libraries/*"],'
+
+TARGET_REGEX='"packages": \[.*\]'
+
+# Выбор замены в зависимости от аргумента
+case $1 in
+  "all")
+    REPLACEMENT=$ALL_PACKAGES
+    ;;
+  "themes")
+    REPLACEMENT=$THEME_PACKAGES
+    ;;
+  "components")
+    REPLACEMENT=$COMPONENT_PACKAGES
+    ;;
+  *)
+    echo "Invalid option."
+    exit 1
+    ;;
+esac
+
+# Чтение файла построчно и замена только нужной строки
+while IFS= read -r line; do
+  if [[ $line =~ $TARGET_REGEX ]]; then
+    echo "    $REPLACEMENT"
+  else
+    echo "$line"
+  fi
+done < "$FILE_PATH" > tmp && mv tmp "$FILE_PATH"
