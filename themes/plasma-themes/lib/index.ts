ebfe59fcc9f16a14ccfa139ea1d71cc673270ede
Neretin Artem <hawktrike@gmail.com>
2024-09-17 21:14:33 +0500

    fix(plasma-themes): Return missed tokens

--- themes/plasma-themes/lib/index.ts
+++ themes/plasma-themes/lib/index.ts
@@ -1,14 +1,7 @@
-// import { generate } from '@salutejs/core-themes-native';
+import { generate } from '@salutejs/core-themes-native';
 
-// generate([
-//     { name: 'plasma_b2c', version: 'latest' },
-//     { name: 'plasma_web', version: 'latest' },
-//     { name: 'stylesSalute', version: 'latest' },
-// ]);
-
-// https://www.npmjs.com/package/@salutejs/plasma-themes-native/v/0.1.0
-// https://www.npmjs.com/package/@salutejs/core-themes-native/v/0.1.0
-// https://www.npmjs.com/package/@salutejs/plasma-b2c-tv/v/0.1.0
-// https://www.npmjs.com/package/@salutejs/core-components-tv/v/0.1.0
-// https://www.npmjs.com/package/@salutejs/plasma-b2c-mobile/v/0.1.0
-// https://www.npmjs.com/package/@salutejs/core-components-mobile/v/0.1.0
+generate([
+    { name: 'plasma_b2c', version: 'latest' },
+    { name: 'plasma_web', version: 'latest' },
+    { name: 'stylesSalute', version: 'latest' },
+]);
