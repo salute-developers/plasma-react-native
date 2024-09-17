9bb25ff8a651748946d770be8ea3501d9cfbe8b2
Neretin Artem <hawktrike@gmail.com>
2024-09-18 01:11:59 +0500

    feat: Update compnents-publish with param and bump versions manual

--- scripts/components-publish.sh
+++ scripts/components-publish.sh
@@ -1,7 +1,7 @@
-npx lerna exec --scope=@salutejs/core-components-$1 -- npm publish --tag alpha
+npx lerna exec --scope=@salutejs/core-components-$1 -- npm publish --tag $2
 
 for dir in $(find "libraries" -maxdepth 2 -type f -name 'package*' -exec dirname {} \; | sort -u); do
   PACKAGE_NAME=$(basename "$dir")
 
-  npx lerna exec --scope=@salutejs/$PACKAGE_NAME-$1 -- npm publish --tag alpha
+  npx lerna exec --scope=@salutejs/$PACKAGE_NAME-$1 -- npm publish --tag $2
 done
