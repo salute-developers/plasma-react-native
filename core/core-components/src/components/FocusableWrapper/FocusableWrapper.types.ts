138ebad0b7cc6999ad75a642594e39b54f095dd5
Neretin Artem <hawktrike@gmail.com>
2024-09-23 23:12:29 +0500

    feat(core-components): Update `FocusableWrapper` component

--- core/core-components/src/components/FocusableWrapper/FocusableWrapper.types.ts
+++ core/core-components/src/components/FocusableWrapper/FocusableWrapper.types.ts
@@ -1,47 +1,17 @@
 import { ReactNode } from 'react';
-import { PressableProps } from 'react-native';
 
-export interface Focusable {
-    focusable?: PressableProps['focusable'];
-    hasTVPreferredFocus?: PressableProps['hasTVPreferredFocus'];
+export interface FocusableWrapperProps {
+    children: ReactNode;
     /**
-     * Данное свойство работает только на TV
+     * Установить цвет для children иконки
      */
-    nextFocusUp?: number;
+    iconColor?: string;
     /**
-     * Данное свойство работает только на TV
+     * Установить`focused=true` для поддерживающего это свойство children компонента
      */
-    nextFocusDown?: number;
-    /**
-     * Данное свойство работает только на TV
-     */
-    nextFocusRight?: number;
-    /**
-     * Данное свойство работает только на TV
-     */
-    nextFocusLeft?: number;
-    /**
-     * Данное свойство работает только на TV
-     */
-    nextFocusForward?: number;
-    onFocus?: PressableProps['onFocus'];
-    onBlur?: PressableProps['onBlur'];
-    onPress?: PressableProps['onPress'];
-    onLongPress?: PressableProps['onLongPress'];
-}
-
-export interface FocusStyle {
-    borderRadius?: number;
-    borderWidth?: number;
-    borderColor?: string;
-}
-
-export interface FocusableWrapperProps extends Omit<PressableProps, 'style'>, Focusable {
-    style?: {
-        root?: PressableProps['style'];
-        focus?: FocusStyle;
-    };
     focused?: boolean;
-    hasFocus?: boolean;
-    children?: ReactNode;
+    /**
+     * Применить любой стиль children компоненту
+     */
+    style?: any;
 }
