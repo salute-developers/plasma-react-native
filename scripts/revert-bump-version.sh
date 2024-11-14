9afd1bb1e6d84293bccfec606c5717b196c972d5
Neretin Artem <hawktrike@gmail.com>
2024-11-14 19:21:40 +0500

    ci: Update workflow for publish icons

--- scripts/revert-bump-version.sh
+++ scripts/revert-bump-version.sh
@@ -1,3 +1,3 @@
-for file in themes/**/package.json themes/**/package-lock.json core/core-themes/package.json core/core-themes/package-lock.json; do
+for file in themes/**/package.json themes/**/package-lock.json core/core-themes/package.json core/core-themes/package-lock.json packages/plasma-icons/package.json packages/plasma-icons/package-lock.json; do
     git checkout -- "$file"
 done
