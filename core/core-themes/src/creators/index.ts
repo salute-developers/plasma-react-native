b54e96810310c017e926cd8c4ee633efd9ee8d6a
Neretin Artem <hawktrike@gmail.com>
2024-12-19 22:15:09 +0700

    feat(core-themes): Add `spacing` tokens generation

--- core/core-themes/src/creators/index.ts
+++ core/core-themes/src/creators/index.ts
@@ -2,5 +2,6 @@ export { createColorTokens } from './createColorTokens';
 export { createGradientTokens } from './createGradientTokens';
 export { createShadowTokens } from './createShadowTokens';
 export { createShapeTokens } from './createShapeTokens';
+export { createSpacingTokens } from './createSpacingTokens';
 export { createFontFamilyTokens } from './createFontFamilyTokens';
 export { createTypographyTokens } from './createTypographyTokens';
