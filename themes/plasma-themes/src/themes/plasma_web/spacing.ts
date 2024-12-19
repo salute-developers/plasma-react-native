ef2ac5208d3a9e9c3c4c3b2f7e4c2898fbba558c
Neretin Artem <hawktrike@gmail.com>
2024-12-19 22:15:52 +0700

    feat(plasma-themes): Add `spacing` tokens to themes

---
+++ themes/plasma-themes/src/themes/plasma_web/spacing.ts
@@ -0,0 +1,34 @@
+export const spacing = {
+    size: {
+        /** spacing 0x */
+        x0: 0,
+        /** spacing 1x */
+        x1: 2,
+        /** spacing 2x */
+        x2: 4,
+        /** spacing 3x */
+        x3: 6,
+        /** spacing 4x */
+        x4: 8,
+        /** spacing 6x */
+        x6: 12,
+        /** spacing 8x */
+        x8: 16,
+        /** spacing 10x */
+        x10: 20,
+        /** spacing 12x */
+        x12: 24,
+        /** spacing 16x */
+        x16: 32,
+        /** spacing 20x */
+        x20: 40,
+        /** spacing 24x */
+        x24: 48,
+        /** spacing 32x */
+        x32: 64,
+        /** spacing 40x */
+        x40: 80,
+        /** spacing 60x */
+        x60: 120,
+    },
+} as const;
--- themes/plasma-themes/src/themes/stylesSalute/index.ts
+++ themes/plasma-themes/src/themes/stylesSalute/index.ts
@@ -4,3 +4,4 @@ export * from './fontFamily';
 export * from './shadow';
 export * from './shape';
 export * from './typography';
+export * from './spacing';
